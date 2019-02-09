a358aef802d6d5d00337be7ef2d73d3f2d379a11
tylerscheuble <tyler@scheuble.us>
2019-02-09 06:02:43 +0000

    Simplified reactive code, removed lib, improved documentation

--- reactive/fresh_rss.py
+++ reactive/fresh_rss.py
@@ -1,28 +1,64 @@
-from charms.reactive import when, when_not, set_flag
-from charms.layer.nginx import configure_site
-from charms.layer import fresh_rss
+from pathlib import Path
+from subprocess import check_call
+
 from charmhelpers.core import hookenv
+from charms.reactive import endpoint_from_flag, when, when_not, set_flag
+from charms.layer.nginx import configure_site
 
 
-config = hookenv.config()
+fresh_rss_dir = Path('/usr/share/FreshRSS')
 
 
-@when_not('fresh_rss.files.fetched')
-def fetch_files():
-    archive = fresh_rss.download_archive()
-    fresh_rss.extract_archive(archive)
-    set_flag('fresh_rss.files.fetched')
+@when_not('fresh_rss.installed')
+def install_freshrss():
+    """Install FreshRSS
+    """
+
+    source = hookenv.resource_get('fresh-rss-tarball')
+    if not source:
+        hookenv.log('Could not find resource fresh-rss-tarball')
+        hookenv.status_set('blocked', "Need fresh-rss-tarball resource")
+        return
+
+    check_call(['mkdir', '-p', str(fresh_rss_dir)])
+    cmd = ['tar', '-zxvf', source, '-C', str(fresh_rss_dir),
+           '--strip-components', '1']
+    hookenv.log('Extracting FreshRSS: {}'.format(' '.join(cmd)))
+    check_call(cmd)
+
+    # set permissions
+    cmd = ['chmod', '-R', 'g+w', str(fresh_rss_dir)]
+    hookenv.log('Setting permissions: {}'.format(''.join(cmd)))
+    check_call(cmd)
+
+    set_flag('fresh_rss.installed')
 
 
-@when('apt.installed.php7.2')
+@when('fresh_rss.installed',
+      'apt.installed.php7.2',
+      'apt.installed.php7.2-fpm',
+      'apt.installed.php7.2-curl',
+      'apt.installed.php7.2-gmp',
+      'apt.installed.php7.2-intl',
+      'apt.installed.php7.2-mbstring',
+      'apt.installed.php7.2-sqlite3',
+      'apt.installed.php7.2-xml',
+      'apt.installed.php7.2-zip')
 @when_not('fresh_rss.nginx.configured')
 def configure_nginx():
+    """Configure NGINX server for fresh_rss
+    """
+
     configure_site('fresh_rss', 'fresh-rss.conf')
+    hookenv.status_set('active', 'ready')
     set_flag('fresh_rss.nginx.configured')
 
-    hookenv.status_set('active', 'ready')
 
+@when('fresh_rss.nginx.configured', 'website.available')
+def configure_website():
+    """Send port data to website endpoint.
+    """
 
-@when('nginx.available', 'website.available')
-def configure_website(website):
-    website.configure(port=config['port'])
+    config = hookenv.config()
+    endpoint = endpoint_from_flag('website.available')
+    endpoint.configure(port=config['port'])
